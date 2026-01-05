eb68a391b353825fb51eb025b88afedf942dfe6f
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-04 19:08:35 -0500

    Step 2: press(write) q to quit

--- kilo.c
+++ kilo.c
@@ -4,6 +4,6 @@ int main() {
 	char c;
 	
 	//read 1 byte from stdin till no bytes to read
-	while(read(STDIN_FILENO, &c, 1) == 1);
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
 }
