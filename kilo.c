2861100869256835499b0c0b5b3ace58f456350d
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-04 20:37:41 -0500

    Step 2: Entering raw mode

--- kilo.c
+++ kilo.c
@@ -3,42 +3,52 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<stdio.h>
+#include<errno.h>
 
 
 struct termios original_terminal_settings;
 
 void enableRawMode();
 void disableRawMode();
+void die();
 
 int main() {
 
 	enableRawMode();
-
-	char c;
 	
 	//read 1 byte from stdin till no bytes to read
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while(1) {
+		char c = '\0';
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 		if(iscntrl(c)) {
-			printf("%d\n",c);
+			printf("%d\r\n",c);
 		}else{
-			printf("%d ('%c')\n", c ,c);
+			printf("%d ('%c')\r\n", c ,c);
 		}
+		if(c == 'q') break;
 	}
 	return 0;
 }
 
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminal_settings);
-
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminal_settings) == -1) {
+		die("tscattr");
+	}
 }
 
 void enableRawMode() {
 
-	tcgetattr(STDIN_FILENO, &original_terminal_settings);
+	if(tcgetattr(STDIN_FILENO, &original_terminal_settings) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios raw = original_terminal_settings;
-	raw.c_lflag &= ~(ECHO | ICANON); 	
+	raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_lflag &= ~(OPOST);
+	raw.c_lflag &= ~(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
+	
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;	
 	// read terminal attributes(keypress)
 	//tcgetattr(STDIN_FILENO, &raw);
 	
@@ -46,5 +56,11 @@ void enableRawMode() {
 	//raw.c_lflag &= ~(ECHO);
 	
 	// apply terminal attributes
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcgetattr");
+}
+
+void die(const char *s) {
+
+	perror(s);
+	exit(1);
 }
