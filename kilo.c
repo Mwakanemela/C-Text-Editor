e9f558a58e36f8d5b7d18abc897bdf74b0a5e082
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-04 19:27:17 -0500

    Step 2: turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -1,9 +1,41 @@
 #include<unistd.h>
+#include<termios.h>
+#include<stdlib.h>
+
+struct termios original_terminal_settings;
+
+void enableRawMode();
+void disableRawMode();
 
 int main() {
+
+	enableRawMode();
+
 	char c;
 	
 	//read 1 byte from stdin till no bytes to read
 	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
+}
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminal_settings);
+
+}
+
+void enableRawMode() {
+
+	tcgetattr(STDIN_FILENO, &original_terminal_settings);
+	atexit(disableRawMode);
+
+	struct termios raw = original_terminal_settings;
+	raw.c_lflag &= ~(ECHO | ICANON); 	
+	// read terminal attributes(keypress)
+	//tcgetattr(STDIN_FILENO, &raw);
+	
+	// modify terminal attributes
+	//raw.c_lflag &= ~(ECHO);
+	
+	// apply terminal attributes
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
