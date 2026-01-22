3e3026228ddaaac763ca3d36ec4ee42a728b809d
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-22 02:49:53 -0500

    Horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow {
 struct editorConfig {
 	int cx, cy;
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -84,6 +85,7 @@ void initializeEditor() {
 	E.cy = 0;
 	E.rowoff = 0;
 	E.numrows = 0;
+	E.coloff = 0;
 	E.row = NULL;
 	
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("Fialed to getWindowSize");
@@ -149,6 +151,14 @@ void editorScroll() {
 	if(E.cy >= E.rowoff + E.screenrows) {
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	
+	//handle horizontal scrolling
+	if(E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if(E.cx >= E.coloff + E.screencols) {
+		E.coloff = E.cx = E.screencols + 1;
+	}
 }
 
 void editorRefreshScreen() {
@@ -167,7 +177,7 @@ void editorRefreshScreen() {
 	editorDrawRows(&ab);
 	
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 	
 	//show cursor
@@ -199,9 +209,10 @@ void editorDrawRows(struct abuf *ab) {
 				abAppend(ab, "~", 1);
 			}
 		}else {
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if(len < 0) len = 0;
 			if(len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if(y < E.screenrows - 1) {
@@ -255,9 +266,7 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_RIGHT: 
-			if(E.cx != E.screencols -1) {
-				E.cx++;
-			}
+			E.cx++;
 			break;
 		case ARROW_UP:
 			if(E.cy != 0) {
