64f53c653e93fe091bd8599672ddf0dfc3aa9844
Mwakanemela <kayangemwakanemela@gmail.com>
2026-01-22 02:53:25 -0500

    Limit scrolling to the right

--- kilo.c
+++ kilo.c
@@ -259,6 +259,7 @@ void editorProcessKeyPress() {
 }
 
 void editorMoveCursor(int key) {
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 	switch(key) {
 		case ARROW_LEFT:
 			if(E.cx != 0) {
@@ -266,7 +267,9 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_RIGHT: 
-			E.cx++;
+			if(row && E.cx < row->size) {
+				E.cx++;
+			}
 			break;
 		case ARROW_UP:
 			if(E.cy != 0) {
